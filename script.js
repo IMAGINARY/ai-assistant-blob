ab7542f24381e689ca7d827f59f4009627c18398
Christian Stussak <christian.stussak@imaginary.org>
2025-04-23 11:36:08 +0200

    Couple movement speed with human proximity
    The proximity is derived from a camera based machine learning body
    pose estimation (ml5.js BodyPose).

--- script.js
+++ script.js
@@ -5,6 +5,24 @@ let envelope = 0;
 
 const loudnessFactor = 50;
 
+let video;
+let bodyPose;
+let poses = [];
+
+const minKeypointConfidence = 0.75;
+
+let nearestZ = Number.POSITIVE_INFINITY;
+const minZ = -0.5;
+const maxZ = -0.2;
+const proximityFactor = 50;
+
+let relativeProximity = 0;
+
+function preload() {
+  // Load the bodyPose model
+  bodyPose = ml5.bodyPose("BlazePose");
+}
+
 function setup() {
   createCanvas(windowWidth, 200);
   userStartAudio();
@@ -12,6 +30,14 @@ function setup() {
   //create & start an audio input
   mic = new p5.AudioIn();
   mic.start();
+
+  // Create the video and hide it
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+
+  // Start detecting poses in the webcam video
+  bodyPose.detectStart(video, gotPoses);
 }
 
 function draw() {
@@ -24,8 +50,64 @@ function draw() {
 
   background(200);
 
-  circle(100, 100, level * loudnessFactor * 100);
+  fill(255);
+  stroke(0);
+
+  // circle(100, 100, level * loudnessFactor * 100);
   circle(300, 100, envelope * loudnessFactor * 100);
+
+  // Draw the webcam video
+  image(video, 400, 0, (200 * 640) / 480, 200);
+
+  // Draw all the tracked landmark points
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      let keypoint = pose.keypoints[j];
+      if (keypoint.confidence < minKeypointConfidence) continue;
+      fill(0, 255, 0);
+      noStroke();
+      circle(
+        400 + ((keypoint.x / 640) * 200 * 640) / 480,
+        (keypoint.y / 480) * 200,
+        10,
+      );
+    }
+  }
+
+  fill(255);
+  stroke(0);
+  circle(400 + (200 * 640) / 480 + 100, 100, relativeProximity * 100);
+
+  fill(0);
+  noStroke();
+  text("Loudness", 200 + 10, 20);
+  text("Bodypose tracking", 400 + 10, 20);
+  text("Proximity", 400 + (200 * 640) / 480 + 10, 20);
+}
+
+// Callback function for when bodyPose outputs data
+function gotPoses(results) {
+  // Save the output to the poses variable
+  poses = results;
+
+  console.log(poses);
+
+  nearestZ = poses.reduce(
+    (minZOuter, { keypoints3D }) =>
+      Math.min(
+        keypoints3D
+          .filter(({ confidence }) => confidence >= minKeypointConfidence)
+          .reduce((minZInner, { z }) => Math.min(minZInner, z), minZOuter),
+        minZOuter,
+      ),
+    Number.POSITIVE_INFINITY,
+  );
+
+  const clampedNearestZ = Math.max(Math.min(nearestZ, maxZ), minZ);
+  relativeProximity = 1.0 - (clampedNearestZ - minZ) / (maxZ - minZ);
+
+  console.log(nearestZ, relativeProximity);
 }
 
 $(document).ready(function () {
@@ -86,7 +168,10 @@ $(document).ready(function () {
   let update = (timestamp) => {
     const timeDiff = timestamp - lastTimestamp;
     lastTimestamp = timestamp;
-    time += timeDiff * 0.00005 * speedSlider.val();
+
+    time += timeDiff * 0.00005 * (1 + relativeProximity * proximityFactor);
+
+    //time += timeDiff * 0.00005 * speedSlider.val();
 
     //    let spikes = spikesSlider.val() * processingSlider.val();
 
